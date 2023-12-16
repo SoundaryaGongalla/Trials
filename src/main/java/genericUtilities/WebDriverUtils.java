28f3fa6536e190b9e84acc12a6d65844a43da416
admin <admin@DESKTOP-FQ2Q8F9>
2023-12-16 14:01:12 +0530

    commit

--- src/main/java/genericUtilities/WebDriverUtils.java
+++ src/main/java/genericUtilities/WebDriverUtils.java
@@ -1,5 +1,15 @@
 package genericUtilities;
 
+import org.openqa.selenium.interactions.Actions;
+
 public class WebDriverUtils {
 
+	public void moveToElement()
+	{
+		Actions act=new Actions(driver);
+				
+				
+	}
+	
+	
 }
